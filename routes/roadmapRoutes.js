c26f22d62b54f84b787bcfd578cf6765fe3c795d
Efty34 <efty17.hasan@gmail.com>
2025-04-06 22:42:20 +0600

    Endpoint testing

--- routes/roadmapRoutes.js
+++ routes/roadmapRoutes.js
@@ -5,6 +5,10 @@ const { generateRoadmap } = require('../controllers/roadmapController');
 
 router.post('/generate', generateRoadmap);
 
+router.get('/generate', (req, res) => {
+    res.status(200).json({ message: 'Roadmap API is working!' });
+});
+
 router.get('/roadmap', (req, res) => {
     res.status(200).json({ message: 'Roadmap endpoint' });
 });
